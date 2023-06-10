8557202361c503666a79ad2f626826aaa1a253e9
TheHellTower <thehelltower@tuta.io>
2023-06-10 12:31:12 -0400

    Small Cleanup

--- src/routes/user.js
+++ src/routes/user.js
@@ -11,19 +11,13 @@ route.get("/:id", async (req, res) => {
   const user = await req.app
     .get("client")
     .users.fetch(req.params.id)
-    .catch((_) => res.render("user/notfound", { userProfile: req.user, req }))
-    //.catch((_) => res.render("404", { req }));
+    .catch((_) => res.render("user/notfound", { userProfile: req.user, req }));
   if (!user) return;
 
   let bots = await Bots.find({}, { Id: false });
   bots = bots.filter((bot) =>
     [bot.owners.primary].concat(bot.owners.additional).includes(user.id)
-  );
-
-  console.log(bots.length);
-  console.log(req.user)
-  console.log(user)
-  
+  );  
 
   if (bots.length === 0)
     return res.render("user/notfound", { userProfile: req.user, req });
